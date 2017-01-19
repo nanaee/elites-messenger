b9389304107ad440dba713f41f9fff4f372e867e
saitonanae <saito-nanae@av.avex.co.jp>
2017-01-19 14:29:05 +0000

    10_実習_メッセージの編集終了しました

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,6 +1,6 @@
 class TimelinesController < ApplicationController
   def index
-    @input_message = Timeline.new
+    @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
     @timeline = Timeline.includes(:user).order('updated_at DESC')
   end
   
@@ -8,6 +8,17 @@ class TimelinesController < ApplicationController
     timeline = Timeline.new
     timeline.attributes = input_message_param
     timeline.user_id = current_user.id
+    if timeline.valid? # バリデーションチェック
+      timeline.save!
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index
+  end
+  
+  def update
+    timeline = Timeline.find(params[:id])
+    timeline.attributes = input_message_param
     if timeline.valid? # バリデーションチェック
       timeline.save!
     else
