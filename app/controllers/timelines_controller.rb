4759d2e6f8f1a0d2ac083cbd05eb0c63f2b067a0
saitonanae <saito-nanae@av.avex.co.jp>
2017-01-20 10:00:44 +0000

    11_実習_投稿ユーザのフィルタリング終了しました

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,8 @@
 class TimelinesController < ApplicationController
   def index
     @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
-    @timeline = Timeline.includes(:user).order('updated_at DESC')
+    @timeline = Timeline.includes(:user).user_filter(params[:filter_user_id]).order('updated_at DESC')
+    @users = User.all
   end
   
   def create
@@ -25,6 +26,15 @@ class TimelinesController < ApplicationController
       flash[:alert] = timeline.errors.full_messages
     end
     redirect_to action: :index
+  end
+  
+  def filter_by_user
+    if params[:filter_user_id].present?
+      redirect_to action: :index, filter_user_id: params[:filter_user_id]
+    else
+      # フィルターなし
+      redirect_to action: :index
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   
-  resources :timelines
+  resources :timelines do
+    collection do
+      post 'filter_by_user'
+    end
+  end
   
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
