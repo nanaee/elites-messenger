3f566c582d0b1234a896846a3ec572065333af64
saitonanae <saito-nanae@av.avex.co.jp>
2017-01-20 10:55:44 +0000

    14_実習_メッセージのAjax投稿まですべて終了しました

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -19,7 +19,15 @@ class TimelinesController < ApplicationController
     else
       flash[:alert] = timeline.errors.full_messages
     end
-    redirect_to action: :index
+    
+    unless request.format.json?
+      redirect_to action: :index
+    else
+      # ajaxの場合のレスポンス
+      html = render_to_string partial: 'timelines/timeline', layout: false, formats: :html, locals: { t: timeline }
+      render json: {timeline: html}
+    end
+    
   end
   
   def update
